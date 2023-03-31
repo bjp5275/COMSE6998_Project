50fac46be1eebaa39681741c9434d34bfaf2d5a5
Ben <bjp5275@gmail.com>
2023-03-30 21:15:56 -0400

    Add simplistic product edit page
    - Use static data
    - No product additions yet

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CustomizeProductComponent } from './customize-product/customize-product.component';
 
-const routes: Routes = [{ path: 'product', component: ProductListComponent }];
+const routes: Routes = [
+  { path: 'product', component: ProductListComponent },
+  { path: 'product-custom', component: CustomizeProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CustomizeProductComponent } from './customize-product/customize-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, CustomizeProductComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
 
     MatButtonModule,
+    MatButtonToggleModule,
     MatCardModule,
     MatGridListModule,
     MatIconModule,
