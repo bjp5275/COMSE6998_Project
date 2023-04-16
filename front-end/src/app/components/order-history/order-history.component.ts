64380b24b79b02c37799fac58fbd735220777126
Ben <bjp5275@gmail.com>
2023-04-16 11:42:04 -0400

    Add reorder validation logic
    - Ensure reorder is adding currently available and valid products,
      additions, and product options
    - Add loading spinner while validating
    - Give user information about validation

--- front-end/src/app/components/order-history/order-history.component.ts
+++ front-end/src/app/components/order-history/order-history.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { Observable, catchError, map, of } from 'rxjs';
-import { Order } from 'src/app/model/models';
+import { Observable, catchError, first, map, of, shareReplay } from 'rxjs';
+import {
+  Order,
+  OrderItem,
+  Product,
+  ProductAddition,
+  convertCoffeeTypeToString,
+  convertMilkTypeToString,
+} from 'src/app/model/models';
 import { CartService } from 'src/app/shared/services/cart.service';
 import { OrderService } from 'src/app/shared/services/order.service';
+import { ProductsService } from 'src/app/shared/services/products.service';
 import { HttpError } from 'src/app/shared/utility';
 import { CustomOrder, OrderAction } from '../order-list/order-list.component';
 
@@ -28,19 +36,21 @@ export class OrderHistoryComponent {
       buttonText: 'Reorder',
       color: 'primary',
       onClick: (orderItems, _index, _order) => {
-        orderItems.forEach((item) => this.cartService.addItem(item));
-        this.router.navigate(['/cart']);
+        this.reorder(orderItems);
       },
     },
   ];
 
   orders$: Observable<CustomOrder<Order>[]>;
+  products$: Observable<Product[] | null>;
+  reordering = false;
 
   constructor(
     orderService: OrderService,
     private cartService: CartService,
+    private productService: ProductsService,
     private router: Router,
-    snackBar: MatSnackBar
+    private snackBar: MatSnackBar
   ) {
     this.orders$ = orderService.getOrderHistory().pipe(
       catchError((err: HttpError) => {
@@ -64,5 +74,157 @@ export class OrderHistoryComponent {
         return customOrders;
       })
     );
+
+    // Get products in anticipation of a reorder and prime the observable for the value
+    this.products$ = this.productService.getProducts().pipe(
+      catchError(() => of(null)),
+      first(),
+      shareReplay()
+    );
+    this.products$.subscribe((products) => {
+      if (!products) {
+        console.log('Failed to load products');
+      }
+    });
+  }
+
+  validateOrderItem(
+    orderItem: OrderItem,
+    index: number,
+    productMap: Map<string, Product>,
+    productAdditionsMap: Map<string, Map<string, ProductAddition>>,
+    errors: string[]
+  ): OrderItem | null {
+    const product = productMap.get(orderItem.productId);
+    if (!product) {
+      errors.push(`Order item ${index + 1} is not available`);
+      return null;
+    }
+
+    const newOrderItem = this.productService.convertToOrderItem(product);
+    if (!product.allowedCoffeeTypes.includes(orderItem.coffeeType)) {
+      const defaultedCoffeeType = product.allowedCoffeeTypes[0];
+      errors.push(
+        `${product.name} is not available with ${convertCoffeeTypeToString(
+          orderItem.coffeeType
+        )} - defaulting to ${convertCoffeeTypeToString(defaultedCoffeeType)}`
+      );
+
+      orderItem.coffeeType = defaultedCoffeeType;
+    }
+    newOrderItem.coffeeType = orderItem.coffeeType;
+
+    const milkType = orderItem.milkType;
+    const allowedMilkTypes = product.allowedMilkTypes;
+    if (
+      (!milkType && allowedMilkTypes?.length) ||
+      (milkType && allowedMilkTypes && !allowedMilkTypes.includes(milkType))
+    ) {
+      const defaultedMilkType = allowedMilkTypes[0];
+      errors.push(
+        `${
+          product.name
+        } requires a valid milk selection - defaulting to ${convertMilkTypeToString(
+          defaultedMilkType
+        )}`
+      );
+      orderItem.milkType = defaultedMilkType;
+    } else if (milkType && !allowedMilkTypes?.length) {
+      errors.push(`${product.name} no longer has a milk selection`);
+      orderItem.milkType = undefined;
+    }
+    newOrderItem.milkType = orderItem.milkType;
+
+    if (orderItem.additions?.length) {
+      const validatedAdditions: ProductAddition[] = [];
+      const allowedAdditions = productAdditionsMap.get(product.id)!;
+
+      orderItem.additions.forEach((addition) => {
+        const validAddition = allowedAdditions.get(addition.id!);
+        if (validAddition) {
+          validatedAdditions.push(validAddition);
+        } else {
+          errors.push(`${product.name} no longer allows ${addition.name}.`);
+        }
+      });
+
+      newOrderItem.additions = validatedAdditions;
+    }
+
+    return newOrderItem;
+  }
+
+  validateOrderItems(
+    orderItems: OrderItem[],
+    products: Product[]
+  ): OrderItem[] {
+    const productMap = new Map<string, Product>();
+    const productAdditionsMap = new Map<string, Map<string, ProductAddition>>();
+    products.forEach((product) => {
+      productMap.set(product.id, product);
+      const additionMap = new Map<string, ProductAddition>();
+      productAdditionsMap.set(product.id, additionMap);
+      product.allowedAdditions?.forEach((addition) =>
+        additionMap.set(addition.id!, addition)
+      );
+    });
+
+    const errors: string[] = [];
+    const validatedOrderItems: OrderItem[] = [];
+    orderItems
+      .map((orderItem, index) =>
+        this.validateOrderItem(
+          orderItem,
+          index,
+          productMap,
+          productAdditionsMap,
+          errors
+        )
+      )
+      .filter((item) => item !== null)
+      .forEach((validOrderItem) => validatedOrderItems.push(validOrderItem!));
+
+    if (errors.length) {
+      const issueMessage = `Issues: \n  -${errors.join('\n  -')}`;
+      this.snackBar.open(issueMessage, 'OK', {
+        duration: 8000,
+        panelClass: ['multiline-snackbar'],
+      });
+    }
+
+    return validatedOrderItems;
+  }
+
+  reorder(orderItems: OrderItem[]) {
+    this.reordering = true;
+
+    // Validate order items are still available
+    this.products$.subscribe({
+      next: (products) => {
+        if (!products) {
+          this.snackBar.open(
+            'Unable to validate products - please try again',
+            'OK'
+          );
+        } else {
+          const validatedOrderItems = this.validateOrderItems(
+            orderItems,
+            products
+          );
+          if (validatedOrderItems.length) {
+            validatedOrderItems.forEach((item) =>
+              this.cartService.addItem(item)
+            );
+            this.router.navigate(['/cart']);
+          } else {
+            this.snackBar.open(
+              'All products in order are no longer available',
+              'OK'
+            );
+          }
+        }
+      },
+      complete: () => (this.reordering = false),
+    });
   }
 }
