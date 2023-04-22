f528729861e5b8a29bb80747f0c1d1169d2d147e
Ben <bjp5275@gmail.com>
2023-04-21 22:09:58 -0400

    Refactor menu logic

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -11,6 +11,25 @@ import {
 } from './shared/services/user.service';
 
 type MenuType = 'MENU';
+type MenuItem = MenuLink | MenuCategory;
+interface _MenuItem {
+  type: string;
+  show?: () => boolean;
+}
+
+interface MenuCategory extends _MenuItem {
+  type: 'CATEGORY';
+  title: string;
+  includeDivider: boolean;
+  links?: MenuLink[];
+}
+
+interface MenuLink extends _MenuItem {
+  type: 'LINK';
+  routerLink: string;
+  icon: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -19,6 +38,77 @@ type MenuType = 'MENU';
 })
 export class AppComponent {
   readonly convertUserRoleToString = convertUserRoleToString;
+  readonly MENU_LINK_TYPE_TEMPLATE!: MenuLink;
+  readonly MENU_CATEGORY_TYPE_TEMPLATE!: MenuCategory;
+  readonly MENU_ITEMS_TYPE_TEMPLATE!: MenuItem[];
+  readonly mainMenuItems: MenuItem[] = [
+    {
+      type: 'LINK',
+      routerLink: '/products',
+      icon: 'coffee',
+      name: 'Products',
+    },
+    {
+      type: 'LINK',
+      routerLink: '/cart',
+      icon: 'shopping_cart',
+      name: 'Cart',
+    },
+    {
+      type: 'CATEGORY',
+      includeDivider: true,
+      title: 'Deliverer Console',
+      show: () => this.actingRole == UserRole.DELIVERER,
+      links: [
+        {
+          type: 'LINK',
+          routerLink: '/delivery/available',
+          icon: 'local_shipping',
+          name: 'Available Deliveries',
+        },
+        {
+          type: 'LINK',
+          routerLink: '/delivery/history',
+          icon: 'history',
+          name: 'Delivery History',
+        },
+      ],
+    },
+    {
+      type: 'CATEGORY',
+      includeDivider: true,
+      title: 'Administrative Console',
+      show: () => this.actingRole == UserRole.ADMIN,
+      links: [
+        {
+          type: 'LINK',
+          routerLink: '/admin/products',
+          icon: 'coffee',
+          name: 'Products',
+        },
+        {
+          type: 'LINK',
+          routerLink: '/admin/additions',
+          icon: 'add_box',
+          name: 'Additions',
+        },
+      ],
+    },
+  ];
+  readonly secondaryMenuItems: MenuItem[] = [
+    {
+      type: 'LINK',
+      routerLink: '/favorites',
+      icon: 'favorite',
+      name: 'Favorites',
+    },
+    {
+      type: 'LINK',
+      routerLink: '/history',
+      icon: 'history',
+      name: 'Order History',
+    },
+  ];
 
   userInformation?: UserInformation;
   actingRole: UserRole = UserRole.REGULAR_USER;
